d7152346335797c0eea0a761f436ce2b154b18a2
mohamedsalem66 <86538514+mohamedsalem66@users.noreply.github.com>
2023-02-12 23:45:32 +0100

    Update WeezzPlayerApplication.java

--- src/main/java/com/frs/weezzplayer/WeezzPlayerApplication.java
+++ src/main/java/com/frs/weezzplayer/WeezzPlayerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableAsync
