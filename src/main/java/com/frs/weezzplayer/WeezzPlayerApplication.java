99fb5067df97603896254daabd5c4a4b5166b266
mohamedsalem66 <86538514+mohamedsalem66@users.noreply.github.com>
2023-02-12 23:43:26 +0100

    Update WeezzPlayerApplication.java

--- src/main/java/com/frs/weezzplayer/WeezzPlayerApplication.java
+++ src/main/java/com/frs/weezzplayer/WeezzPlayerApplication.java
@@ -12,8 +12,7 @@ public class WeezzPlayerApplication {
 
     public static void main(String[] args) {
         ApplicationContext cnt = SpringApplication.run(WeezzPlayerApplication.class, args);
-        CampaignElementRepository campaignelementrepository = cnt.getBean(CampaignElementRepository.class);
-    	campaignelementrepository.save(new CampaignElement());
+        
     }
 
 }
